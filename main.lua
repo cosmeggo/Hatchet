7038b93d503e1be2b3eeda40e6824e05bdde7559
cosmeggo <cosmeggo@gmail.com>
2026-05-19 23:26:53 +0100

    Updated to v2.0.0-BETA-2

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+Hatchet = SMODS.current_mod
+
 --ATLASES
 SMODS.Atlas({
     key = "modicon", 
@@ -18,144 +20,119 @@ SMODS.Atlas({
 
 
 SMODS.Atlas({
-    key = "CustomJokers", 
-    path = "CustomJokers.png", 
+    key = "HatchetJokers", 
+    path = "HatchetJokers.png", 
     px = 71,
     py = 95, 
     atlas_table = "ASSET_ATLAS"
 })
 
 SMODS.Atlas({
-    key = "CustomConsumables", 
-    path = "CustomConsumables.png", 
+    key = "HatchetConsumables", 
+    path = "HatchetConsumables.png", 
     px = 71,
     py = 95, 
     atlas_table = "ASSET_ATLAS"
 })
 
 SMODS.Atlas({
-    key = "CustomBoosters", 
-    path = "CustomBoosters.png", 
+    key = "HatchetBoosters", 
+    path = "HatchetBoosters.png", 
     px = 71,
     py = 95, 
     atlas_table = "ASSET_ATLAS"
 })
 
 SMODS.Atlas({
-    key = "CustomEnhancements", 
-    path = "CustomEnhancements.png", 
+    key = "HatchetEnhancements", 
+    path = "HatchetEnhancements.png", 
     px = 71,
     py = 95, 
     atlas_table = "ASSET_ATLAS"
 })
 
 SMODS.Atlas({
-    key = "CustomDecks", 
-    path = "CustomDecks.png", 
+    key = "HatchetDecks", 
+    path = "HatchetDecks.png", 
     px = 71,
     py = 95, 
     atlas_table = "ASSET_ATLAS"
 })
 
 SMODS.Atlas({
-    key = "CustomStickers", 
-    path = "CustomStickers.png", 
+    key = "HatchetStickers", 
+    path = "HatchetStickers.png", 
     px = 71,
     py = 95, 
     atlas_table = "ASSET_ATLAS"
 })
 
 SMODS.Atlas({
-    key = "CustomStakes", 
-    path = "CustomStakes.png",
+    key = "HatchetStakes", 
+    path = "HatchetStakes.png",
     px = 29,
     py = 29, 
     atlas_table = "ASSET_ATLAS"
 })
 
 SMODS.Atlas({
-    key = "CustomBlinds", 
-    path = "CustomBlinds.png", 
+    key = "HatchetBlinds", 
+    path = "HatchetBlinds.png", 
     px = 34,
     py = 34, 
     frames = 21, 
     atlas_table = "ANIMATION_ATLAS"
 })
 
+SMODS.Atlas({ -- Crossmod Content
+    key = "HatchetCrossJokers", 
+    path = "HatchetCrossJokers.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
 --loading src files
+assert(SMODS.load_file("src/backs.lua"))()
 assert(SMODS.load_file("src/blinds.lua"))()
-assert(SMODS.load_file('src/jokers.lua'))()
-assert(SMODS.load_file("src/c_sephirot.lua"))()
-assert(SMODS.load_file("src/c_divine.lua"))()
+assert(SMODS.load_file("src/boosters.lua"))()
+assert(SMODS.load_file("src/challenges.lua"))()
+assert(SMODS.load_file("src/divine.lua"))()
+assert(SMODS.load_file("src/editions.lua"))()
+assert(SMODS.load_file("src/enhancements.lua"))()
+assert(SMODS.load_file("src/jokers.lua"))()
+assert(SMODS.load_file("src/rarities.lua"))()
+assert(SMODS.load_file("src/sephirot.lua"))()
 assert(SMODS.load_file("src/sounds.lua"))()
 assert(SMODS.load_file("src/stakes.lua"))()
-assert(SMODS.load_file("src/backs.lua"))()
-assert(SMODS.load_file("src/enhancements.lua"))()
-assert(SMODS.load_file("src/editions.lua"))()
-assert(SMODS.load_file("src/challenges.lua"))()
 assert(SMODS.load_file("src/stickers.lua"))()
-assert(SMODS.load_file("src/boosters.lua"))()
+assert(SMODS.load_file("src/crossmod.lua"))()
 
+-- Consumable Definitions
 SMODS.ConsumableType {
-    key = 'divine',
-    primary_colour = HEX('484cdb'),
-    secondary_colour = HEX('484cdb'),
-    collection_rows = { 4, 5 },
-    shop_rate = 0,
-    cards = {
-        ['c_hatch_daat'] = true,
-        ['c_hatch_soulofhatchet'] = true
-    },
-    loc_txt = {
-        name = "Divine",
-        collection = "Divine Cards",
-    }
-}
-
-SMODS.ConsumableType {
-    key = 'sephirot',
+    key = 'hatch_sephirot',
+    default = 'c_hatch_kether',
     primary_colour = HEX('0ebab2'),
     secondary_colour = HEX('0ebab2'),
-    collection_rows = { 4, 5 },
+    collection_rows = { 5, 6 },
     shop_rate = 0,
-    cards = {
-        ['c_hatch_kether'] = true,
-        ['c_hatch_chokmah'] = true,
-        ['c_hatch_binah'] = true,
-        ['c_hatch_chesed'] = true,
-        ['c_hatch_gevurah'] = true,
-        ['c_hatch_tiferet'] = true,
-        ['c_hatch_netzach'] = true,
-        ['c_hatch_hod'] = true,
-        ['c_hatch_yesod'] = true,
-        ['c_hatch_malkuth'] = true
-    },
     loc_txt = {
         name = "Sephirot",
         collection = "Sephirot Cards",
     }
 }
 
-SMODS.current_mod.optional_features = function()
-    return {
-        cardareas = {} 
-    }
-end
-
--- rarities
-SMODS.Rarity {
-    key = "evolved",
-    pools = {
-        ["Joker"] = true
-    },
-    default_weight = 0,
-    badge_colour = HEX('4a90e2'),
+SMODS.ConsumableType {
+    key = 'hatch_divine',
+    default = 'c_hatch_daat',
+    primary_colour = HEX('484cdb'),
+    secondary_colour = HEX('484cdb'),
+    collection_rows = { 5, 6 },
+    shop_rate = 0,
     loc_txt = {
-        name = "Evolved"
-    },
-    get_weight = function(self, weight, object_type)
-        return weight
-    end,
+        name = "Divine",
+        collection = "Divine Cards",
+    }
 }
 
 --- Main Menu Colours (Credit to Cryptid and JoyousSpring)
@@ -178,8 +155,18 @@ function Game:main_menu(change_context)
     return ret
 end
 
--- Credit to NopeTooFast
+-- SMODS Optional Features
+SMODS.current_mod.optional_features = {
+    retrigger_joker = true,
+    post_trigger = true,
+    quantum_enhancements = true,
+    cardareas = {
+        discard = true,
+        deck = true
+    }
+}
 
+-- Credit to NopeTooFast
 SMODS.current_mod.menu_cards = function()
 return {
   { key = 'j_hatch_hatchet' },
